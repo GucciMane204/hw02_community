35468173b45e81e9ce8916dc857806f039278a96
Gucci Mane <mimidor204@gmail.com>
2023-03-08 13:17:34 +0300

    Добавлен пожилой paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,22 +1,28 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-POSTS = 10
-
 
 def index(request):
-    posts = Post.objects.select_related()[:POSTS]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POSTS]
+    post_list = Post.objects.filter(group=group).order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'posts': post_list,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
