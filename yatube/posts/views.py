dbffcc0259c272951e0f1d0c0dede35881bcb51c
Gucci Mane <mimidor204@gmail.com>
2023-03-08 20:31:12 +0300

    Персональная страница пользователя и страница записи

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 
 
 def index(request):
@@ -26,3 +26,34 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=author).order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    count_posts = author.posts.count()
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "page_obj": page_obj,
+        "count_posts": count_posts,
+        "author": author,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    pub_date = post.pub_date
+    post_title = post.text[:30]
+    author = post.author
+    author_posts = author.posts.count()
+    context = {
+        "post": post,
+        "post_title": post_title,
+        "author": author,
+        "author_posts": author_posts,
+        "pub_date": pub_date,
+    }
+    return render(request, 'posts/post_detail.html', context)
